d683b1ee98d799a8d62cc6854052cf077ac257c3
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2018-02-27 17:19:54 +0100

    added tagging to script

--- .scripts/check-deploy.sh
+++ .scripts/check-deploy.sh
@@ -11,3 +11,5 @@ else
     else  
   fi
 fi
+
+aws s3api put-object-tagging --bucket ${AWS_PATH} --key rocketchat/${BUILD_FILE --tagging "{ \"TagSet\": [ { \"Key\": \"environment\", \"Value\": \"${TARGET_ENVIRONMENT}\" }, { \"Key\": \"nodejs_version\", \"Value\": \"${NODEJS_VERSION}\" }, { \"Key\": \"nodejs_checksum\", \"Value\": \"${NODEJS_CHECKSUM}\", { \"Key\": \"assets\", \"Value\": \"${ASSETS_URL}\" } ] }"
